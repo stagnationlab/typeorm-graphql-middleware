884019078c7fe9638609eba37a7fa6d47a329928
Taavi Sangel <taavi@stagnationlab.com>
2018-01-22 19:56:07 +0200

    updated build

--- build/services/create-schema.js
+++ build/services/create-schema.js
@@ -7,7 +7,9 @@ const merge_graphql_schemas_1 = require("merge-graphql-schemas");
 const get_type_definitions_1 = require("./get-type-definitions");
 let cachedContent = '';
 exports.default = (typeDefsPath, outputPath) => {
-    const typeDefs = get_type_definitions_1.default(typeDefsPath).map(def => def.replace(/ extend/g, ''));
+    let typeDefs = get_type_definitions_1.default(typeDefsPath);
+    typeDefs = typeDefs.map(def => def.replace(/\s*extend\s*/g, ''));
+    console.log(typeDefs);
     const schema = merge_graphql_schemas_1.mergeTypes(typeDefs);
     if (cachedContent !== schema) {
         mkdirp.sync(path.dirname(outputPath));
